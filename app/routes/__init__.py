d7a85056c0cc8192b4e49b234a2a671a115def4c
victoriaDunlap <victoriacdunlap@gmail.com>
2022-09-29 10:29:05 -0400

    routes for homepage and dashboard added

---
+++ app/routes/__init__.py
@@ -0,0 +1,4 @@
+# the .home directs the program to find the home module in the current directory
+# next import the bp object and rename it home 
+from .home import bp as home
+from .dashboard import bp as dashboard
