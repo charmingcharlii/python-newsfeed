f218fd29b601d3c604c31e5b4d5db2ffd14bd56a
victoriaDunlap <victoriacdunlap@gmail.com>
2022-10-10 13:57:55 -0400

    error handling added

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
+import sys
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -10,16 +11,24 @@ def signup():
     data = request.get_json()
     db = get_db()
     
-    # create a new user 
-    newUser = User(
-        username = data['username'],
-        email = data['email'],
-        password = data['password']
-    )
+    try:
+        # create a new user 
+        newUser = User(
+            username = data['username'],
+            email = data['email'],
+            password = data['password']
+        )
 
-    # save in database -- we use the db.add() method to prep the INSERT statement and the db.commit() method to officially update the database
-    db.add(newUser)
-    db.commit()
+        # save in database -- we use the db.add() method to prep the INSERT statement and the db.commit() method to officially update the database
+        db.add(newUser)
+        db.commit()
+
+    except:
+        print(sys.exc_info()[0])
+
+        # insert failed, so rollback and send error to front end
+        db.rollback()
+        return jsonify(message = 'Signup failed'), 500
 
     return jsonify(id = newUser.id)
 
